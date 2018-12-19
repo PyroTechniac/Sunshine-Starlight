bca499800ca73c7eed1cb4bcf037273e2bb09dd1
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2018-12-18 20:36:26 -0500

    Initial add -- config work

---
+++ Sunshine-Starlight/config.js
@@ -0,0 +1,36 @@
+﻿require("dotenv").config()
+const convict = require("convict")
+const fs = require("fs")
+const config = convict({
+    env: {
+        doc: "The application environment",
+        format: ['prod', 'dev', 'test'],
+        default: "dev",
+        env: "NODE_ENV",
+        arg: 'env'
+    },
+    bots: {
+        starlight: {
+            name: {
+                doc: "Starlight's name",
+                format: String,
+                default: "Starlight",
+                env: "NAME_STAR",
+                arg: "name-star"
+            },
+            token: {
+                doc: "Starlight's token",
+                format: String,
+                default: "",
+                sensitive: true
+                env: "TOKEN_STAR",
+                arg: 'token-star'
+            }
+        },
+        sunshine: {
+            name: {
+                doc: "Sunshine's name"
+            }
+        }
+    }
+})
